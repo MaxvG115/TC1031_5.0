b182cea7200ec65b951f6a569c5154af9b189708
Maximiliano Villegas <73923032+MaxvG115@users.noreply.github.com>
2021-11-26 17:09:29 -0600

    Actualizacion Autores

--- MyHashTable.h
+++ MyHashTable.h
@@ -1,3 +1,12 @@
+/*
+===============================*
+<Autores>
++Vicente Viera Guízar, ID: A01639784
++Maximiliano Villegas Garcia. ID: A01635825
++Emma Gabriela Alfaro De la Rocha. ID: A01740229
+</Autores>
+=================================*
+*/
 #ifndef MYHASHTABLE_H
 #define MYHASHTABLE_H
 #include <list>
