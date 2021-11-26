2619924d19ec9ff04d3c1e1fd559aff8216ff63d
Maximiliano Villegas <73923032+MaxvG115@users.noreply.github.com>
2021-11-26 17:12:40 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -27,7 +27,7 @@ int main(){
     ht.get(IP);
     cout << "\n" << "++++ presione 0 para salir o 1 para continuar: ++++" << endl;
     cin>>cont;
-    cout << endl;  
+    cout << "//////////////////////////////////////////" << endl; 
   }while(cont);
 
   return 0;
