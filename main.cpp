6082b9607073aa1d0a2e1c9c8665e0fe560d81a9
Maximiliano Villegas <73923032+MaxvG115@users.noreply.github.com>
2021-11-26 17:08:15 -0600

    Actualizacion de formato y autores

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
+/*
+===============================*
+<Autores>
++Vicente Viera Guízar, ID: A01639784
++Maximiliano Villegas Garcia. ID: A01635825
++Emma Gabriela Alfaro De la Rocha. ID: A01740229
+</Autores>
+=================================*
+*/
 #include <iostream>
 #include "MyHashTable.h"
 
 using namespace std;
 
-
+//main con menu de pruebas
 int main(){
 
   MyHashTable ht;
@@ -12,11 +21,13 @@ int main(){
   string IP;
 
   do{
-    cout << "Ingrese una IP" << endl;
+    cout << "++++++++ Ingrese una direccion IP ++++++++" << endl;
     cin >> IP;
+    cout << "\n" << endl;
     ht.get(IP);
-    cout<<"presione 0 para salir o 1 para continuar: "<<endl;
-    cin>>cont;  
+    cout << "\n" << "++++ presione 0 para salir o 1 para continuar: ++++" << endl;
+    cin>>cont;
+    cout << endl;  
   }while(cont);
 
   return 0;
